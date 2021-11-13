da130076864ae7a7f4a0e070e85bdded7c19ea56
Quarry9221 <vadim1zolotko@gmail.com>
2021-11-13 12:02:45 +0200

    New branch

---
+++ DAL/Entities/OSN.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace DAL.Entities
+{
+    public class OSN
+    {
+        public int OSBBID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Director { get; set; }
+
+        public List<Street> Streets { get; set; }
+    }
+}

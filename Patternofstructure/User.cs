a78a6fe5d86ef8e4a089ae1170f90080e84c69f4
Quarry9221 <vadim1zolotko@gmail.com>
2021-12-04 11:52:26 +0200

    Added User to Structure Pattern

---
+++ Patternofstructure/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DAL.Entities;
+namespace Patternofstructure
+{
+    class User
+    {
+        static void Main(string[] args)
+        {
+            Facade facade = new Facade(new OSN(), new Street());
+            facade.Operation();
+            facade.Operation2();
+            facade.Finish();
+        }
+    }
+}
